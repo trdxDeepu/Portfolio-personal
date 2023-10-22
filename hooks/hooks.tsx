35dda1c9d1387632e6a567156542e15d79996b8f
trdxlucifer <singhdepu566@gmail.com>
2023-10-22 12:47:28 +0530

    final commit

--- hooks/hooks.tsx
+++ hooks/hooks.tsx
@@ -5,7 +5,7 @@ import { useActiveSectionContext } from '@/context/active-section-context'
 import { useInView } from 'react-intersection-observer'
 import type { SectionName } from '@/lib/types'
 
-export function useSectionInView (sectionName: SectionName,threshold = 0.2) {
+export function useSectionInView (sectionName: SectionName,threshold=0.1) {
   const { ref, inView } = useInView({threshold})
   const { setActiveSection, timeofLastClick } = useActiveSectionContext()
 
