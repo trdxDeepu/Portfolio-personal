e30ed0ae6da40efb712f7511fae2a1680dece7c8
trdxlucifer <singhdepu566@gmail.com>
2023-10-22 12:42:22 +0530

    final commit

--- components/contact.jsx
+++ components/contact.jsx
@@ -11,7 +11,7 @@ import { sendEmail } from '@/actions/sendemail'
 import { useSectionInView } from '@/hooks/hooks'
 
 export default function Contact () {
-  const { ref } = useSectionInView('Contact',0.6)
+  const { ref } = useSectionInView('Contact',0.4)
 
   return (
     <motion.section
