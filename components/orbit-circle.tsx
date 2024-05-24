9713968925b711a3c2f4aba15d32d20a8a3ab04d
depu <singhdepu566@gmail.com>
2024-05-24 12:31:55 +0530

    change orbit circles

--- components/orbit-circle.tsx
+++ components/orbit-circle.tsx
@@ -6,7 +6,7 @@ export function OrbitingCirclesDemo() {
   return (
     <div className="relative flex h-[500px] w-full max-w-[32rem] items-center justify-center overflow-hidden rounded-lg  bg-background ">
       <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
-        Circles
+        Skills
       </span>
 
       {/* Inner Circles */}
